c3028f1a12ee0d998c1deca659a32cce6faefbe1
henriquesantosgit <164537714+henriquesantosgit@users.noreply.github.com>
2024-08-22 14:26:00 -0300

    Soma de matrizes

--- main.java
+++ main.java
@@ -1,43 +1,36 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        
-        // Matriz 6x2 
-        String[][] matriz = {
-            {"usuario1", "senha1"},
-            {"usuario2", "senha2"},
-            {"usuario3", "senha3"},
-            {"usuario4", "senha4"},
-            {"usuario5", "senha5"},
-            {"usuario6", "senha6"}
+    
+
+        int[][] matrizA = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
         };
 
-        // Solicita ao usuário o login e a senha
-        System.out.println("Digite o usuário: ");
-        String usuario = scan.nextLine();
-        
-        System.out.println("Digite a senha: "); 
-        String senha = scan.nextLine();
-    //precisa iniciar uma booleana ,é necessário
-        boolean valido = false;
-        
-   //
-        for (int i = 0; i < matriz.length; i++) {
-            if (usuario.equals(matriz[i][0]) && senha.equals(matriz[i][1])) {
-                valido = true;
-                break; // Se encontrou, não precisa continuar verificando
+        int[][] matrizB = {
+            {9, 8, 7},
+            {6, 5, 4},
+            {3, 2, 1}
+        };
+
+        // Matriz resultado ,armazena o resultado da soma
+        int[][] matrizResultado = new int[3][3];
+
+       //i para linhas ,j para colunas
+        for (int i = 0; i < TAMANHO; i++) {
+            for (int j = 0; j < TAMANHO; j++) {
+                matrizResultado[i][j] = matrizA[i][j] + matrizB[i][j];
             }
         }
 
-        // Exibe o resultado para o usuário
-        if (valido) {
-            System.out.println("Acesso permitido");
-        } else {
-            System.out.println("Acesso negado");
+        // Exibir o resultado
+        System.out.println("Resultado:");
+        for (int i = 0; i < TAMANHO; i++) {
+            for (int j = 0; j < TAMANHO; j++) {
+                System.out.print(matrizResultado[i][j] + " ");
+            }
+            System.out.println();
         }
-
-        
     }
 }
